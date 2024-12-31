e1234892a1375f7435b32510123cf3f1722b928e
vishnu prasanth <kvishnuprasanth2@gmail.com>
2024-12-31 11:52:09 +0530

    ecomm

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const homeController=require('../controllers/home_controller')
-// const passport=require('passport')
-
-router.use('/api',require('./api'));
-router.use('/',homeController.home)
-
-module.exports=router;
